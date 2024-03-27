589b6096fbbda742ba2a3713cca11ee9bb807e79
Eduardo Barrera III <edbar1025@gmail.com>
2024-03-27 22:02:38 +0800

    NS-17 add notification for email

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,12 @@ export class Config {
   static get clicksendAuth(): string {
     return process.env.CLICKSEND_AUTH || '';
   }
+
+  static get mailerApiKey(): string {
+    return process.env.MAILERSEND_API_KEY || '';
+  }
+
+  static get mailerEmailDomain(): string {
+    return process.env.MAILERSEND_SENDER_DOMAIN || '';
+  }
 }
--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -7,16 +7,22 @@ import {
   HttpStatus,
 } from '@nestjs/common';
 import { ClicksendService } from '../notification/sms/clicksend/clicksend.service';
+import {
+  EmailData,
+  MailersendService,
+} from '../notification/email/mailersend/mailersend.service';
 @Controller('/api/v1/notification')
 export class NotificationController {
-  constructor(private readonly clicksendService: ClicksendService) {}
+  constructor(
+    private readonly clicksendService: ClicksendService,
+    private readonly mailersendService: MailersendService,
+  ) {}
 
-  @Post('send')
+  @Post('sendSMS')
   async sendSMSNotification(
-    @Body() body: { channel: string; content: object },
+    @Body() body: { channel: string; content: object | EmailData },
     @Res() res,
   ): Promise<any> {
-    console.log('Sending SMS notification to', JSON.stringify(body));
     if (!body.channel || !body.content) {
       throw new HttpException('Invalid input', HttpStatus.BAD_REQUEST);
     }
@@ -36,8 +42,30 @@ export class NotificationController {
           HttpStatus.INTERNAL_SERVER_ERROR,
         );
       }
-    } else {
-      throw new HttpException('Invalid channel', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @Post('sendEmail')
+  async sendEmailNotification(
+    @Body() body: { channel: string; content: EmailData },
+    @Res() res,
+  ): Promise<any> {
+    if (!body.content) {
+      throw new HttpException('Invalid input', HttpStatus.BAD_REQUEST);
+    }
+    try {
+      const response = await this.mailersendService.sendEmail(body.content);
+      if (response) {
+        return res.status(HttpStatus.CREATED).json({
+          statusCode: HttpStatus.ACCEPTED,
+          message: 'Email notification sent successfully',
+        });
+      }
+    } catch (error) {
+      throw new HttpException(
+        'Failed to send SMS',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
